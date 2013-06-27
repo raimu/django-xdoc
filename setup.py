89c6af1abf79f7781ded6bbc48d118b494deaadd
raimu <ortwang@googlemail.com>
2013-06-27 15:26:08 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='django-xdoc',
+    version='0.0.1',
+    packages=['xdoc'],
+    url='https://github.com/raimu/django-xdoc',
+    license='GPL 3',
+    author='raimu',
+    author_email='ortwang@googlemail.com',
+    description='document-management-system'
+)
