9d558d85c7a5dfa1d6cae4e95ec5628ca917f84a
raimu <ortwang@googlemail.com>
2013-11-20 22:16:06 +0100

    simplify install

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
 from distutils.core import setup
 
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+print required
+
 setup(
     name='django-xdoc',
     version='0.0.1',
     packages=['xdoc'],
+    include_package_data=True,
+    zip_safe=False,
     url='https://github.com/raimu/django-xdoc',
-    license='GPL 3',
+    license='AGPL 3',
+    install_requires=required
 )
--- testproject/settings.py
+++ testproject/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # my apps
     'xdoc',
     'rest_framework',
     'crispy_forms',
